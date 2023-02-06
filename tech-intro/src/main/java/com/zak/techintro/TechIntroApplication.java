de1cee78467c0be87afd177c612f7214fc03af54
ZakCannon <zak.cannon@softwire.com>
2023-02-06 14:56:27 +0000

    Split into java/tsx

---
+++ tech-intro/src/main/java/com/zak/techintro/TechIntroApplication.java
@@ -0,0 +1,13 @@
+package com.zak.techintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechIntroApplication.class, args);
+	}
+
+}
---
+++ tech-intro/src/test/java/com/zak/techintro/TechIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zak.techintro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
